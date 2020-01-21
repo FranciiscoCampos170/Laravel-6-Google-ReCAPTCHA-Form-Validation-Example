293c2b746e9e0f2023a8733758d644aa67492c93
Francisco Campos <franciiscocampos170@gmail.com>
2020-01-21 23:28:21 +0100

    Configure controller file

---
+++ app/Http/Controllers/SiteAuthController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SiteAuthController extends Controller
+{
+    public function siteRegister()
+    {
+        return view('siteRegister');
+    }
+
+    public function siteRegisterPost(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required|same:password_confirmation',
+            'password_confirmation' => 'required',
+            'g-recaptcha-response' => 'required|captcha',
+        ]);
+
+        print('done');
+    }
+
+}
